2832515b1b501ec71192376331aff5e6d2c2bd3f
Aman Marpu <amarpu@purdue.edu>
2026-02-06 16:58:34 -0500

    Lab 2Step 3

---
+++ ws2/src/rrbot_description/setup.py
@@ -0,0 +1,27 @@
+import os
+from glob import glob
+from setuptools import setup
+
+package_name = 'rrbot_description'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='amarpu',
+    maintainer_email='amarpu@purdue.edu',
+    description='RRBot description package',
+    license='TODO',
+)
+
